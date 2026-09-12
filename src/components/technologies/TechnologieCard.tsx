6813bf2340d0b76ab727c7a160f280a42cdcc59f
Mr_kulukulu <mazumdertonoy582@gmail.com>
2026-09-13 01:10:59 +0600

    button fixed

--- src/components/technologies/TechnologieCard.tsx
+++ src/components/technologies/TechnologieCard.tsx
@@ -1,73 +1,80 @@
-
-import { toast } from 'react-toastify';
-import type ITechnology from '../../types/type';
+import { toast } from "react-toastify";
+import type ITechnology from "../../types/type";
+import { FaCheck } from "react-icons/fa";
 
 interface TechnologieCardProps {
-    technology: ITechnology;
-    selectedTechnologies: ITechnology[];
-    setSelectedTechnologies: React.Dispatch<React.SetStateAction<ITechnology[]>>;
+  technology: ITechnology;
+  selectedTechnologies: ITechnology[];
+  setSelectedTechnologies: React.Dispatch<React.SetStateAction<ITechnology[]>>;
 }
-const TechnologieCard = ({ technology, selectedTechnologies, setSelectedTechnologies }: TechnologieCardProps) => {
-    const isSelected = selectedTechnologies.some(
-        (selectedTechnology) => selectedTechnology.id === technology.id
-    );
-    const handleAddToStack = () => {
-        setSelectedTechnologies([...selectedTechnologies, technology]);
-        toast.success(`${technology.name} added to stack`)
-    }
-    return (
-        <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-300 border border-gray-100">
-            <div>
-                <div className="flex items-center justify-between gap-2">
-                    <img
-                        className="h-9 w-9 object-contain"
-                        src={technology.icon}
-                        alt={technology.name}
-                    />
+const TechnologieCard = ({
+  technology,
+  selectedTechnologies,
+  setSelectedTechnologies,
+}: TechnologieCardProps) => {
+  const isSelected = selectedTechnologies.some(
+    (selectedTechnology) => selectedTechnology.id === technology.id,
+  );
+  const handleAddToStack = () => {
+    setSelectedTechnologies([...selectedTechnologies, technology]);
+    toast.success(`${technology.name} added to stack`);
+  };
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-300 border border-gray-100">
+      <div>
+        <div className="flex items-center justify-between gap-2">
+          <img
+            className="h-9 w-9 object-contain"
+            src={technology.icon}
+            alt={technology.name}
+          />
 
-                    <p className="text-sm font-medium text-gray-600 rounded-2xl bg-gray-100 px-3 py-1">
-                        {technology.badge}
-                    </p>
-                </div>
-
-                <h3 className="font-bold text-lg mt-3 text-gray-900">
-                    {technology.name}
-                </h3>
-
-                <p className="text-gray-600 mt-1 leading-6">
-                    {technology.description}
-                </p>
-            </div>
-
-            <div className="mt-4 flex justify-between items-center">
-                <p className="text-sm font-medium text-blue-600 bg-blue-50 px-2 py-1 rounded-md">
-                    {technology.category}
-                </p>
-
-                <p className="text-sm text-gray-500">
-                    {technology.difficulty}
-                </p>
-
-                <p className="font-semibold text-gray-800">
-                    ⭐ {technology.rating}
-                </p>
-            </div>
-
-            <div className="flex justify-center">
-                <button
-                    onClick={handleAddToStack}
-                    disabled={isSelected}
-                    className={`mt-4 w-full text-white font-bold py-2 px-4 rounded-lg transition-all duration-300 ${isSelected
-                            ? "bg-gray-400 cursor-not-allowed"
-                            : "bg-black hover:bg-gray-800 hover:scale-[1.02]"
-                        }`}
-                >
-                    {isSelected ? "Added" : "Add to Stack"}
-                </button>
-            </div>
-
+          <p className="text-sm font-medium text-gray-600 rounded-2xl bg-gray-100 px-3 py-1">
+            {technology.badge}
+          </p>
         </div>
-    );
-}
+
+        <h3 className="font-bold text-lg mt-3 text-gray-900">
+          {technology.name}
+        </h3>
+
+        <p className="text-gray-600 mt-1 leading-6">{technology.description}</p>
+      </div>
+
+      <div className="mt-4 flex justify-between items-center">
+        <p className="text-sm font-medium text-blue-600 bg-blue-50 px-2 py-1 rounded-md">
+          {technology.category}
+        </p>
+
+        <p className="text-sm text-gray-500">{technology.difficulty}</p>
+
+        <p className="font-semibold text-gray-800">⭐ {technology.rating}</p>
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          onClick={handleAddToStack}
+          disabled={isSelected}
+          className={`mt-4 w-full text-white font-bold py-2 px-4 rounded-lg 
+      transition-all duration-300 flex items-center justify-center gap-2
+      ${
+        isSelected
+          ? "bg-gray-400 cursor-not-allowed"
+          : "bg-black hover:bg-gray-800 hover:scale-[1.02]"
+      }`}
+        >
+          {isSelected ? (
+            <>
+              <FaCheck />
+              <span>Added to Stack</span>
+            </>
+          ) : (
+            "Add to Stack"
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TechnologieCard;
